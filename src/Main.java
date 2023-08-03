7dd0d772b56af274fe2361be187a8ed7d16203ef
ZulemaButler <zxbutl3r@gmail.com>
2023-08-02 20:32:27 -0400

    final commit

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,17 @@
 public class Main {
     public static void main(String[] args) {
 
-       int num1=2;
-       int num2=2;
-               int result=num1-num2;
-        System.out.println(result);
+        boolean productAvailable=true;
 
-        }
+        if ( productAvailable) {
+            System.out.println( "yes");
+        } else {
+                System.out.println("no");
+            }
+    }
+
+
+
+
 
 }
